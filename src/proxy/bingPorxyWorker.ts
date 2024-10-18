9fa4c77f4ff9bfb76d171a091e847508a0badbb0
简简爱 <2801819967@qq.com>
2024-10-18 07:38:33 +0000

    yes

--- src/proxy/bingPorxyWorker.ts
+++ src/proxy/bingPorxyWorker.ts
@@ -1,7 +1,27 @@
+/** CORS */
+function handleOptions(request: Request) {
+    let url = new URL(request.url);
+    const corsHeaders = {
+        // 'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Origin': request.headers.get("Origin") || "",
+        'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+        'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers') || '',
+        'Access-Control-Max-Age': '86400',
+        'Access-Control-Allow-Credentials': 'true'
+    };
+    return new Response(null, { headers: corsHeaders });
+}
+
 export const bingPorxyWorker = async (req: Request,env:Env):Promise<Response>=>{
+    console.log(req.url);
+    if(req.method=="OPTIONS"){
+        return handleOptions(req);
+    }
     // 开始请求
     const url = new URL(req.url);
     const headers = new Headers(req.headers);
+    headers.delete("host");
+    headers.delete("x-forwarded-host")
     const fip = headers.get("MCPXXX-FIP");
     headers.delete("MCPXXX-FIP");
     const toHost = headers.get("MCPXXX-TO-HOST");
@@ -14,7 +34,11 @@ export const bingPorxyWorker = async (req: Request,env:Env):Promise<Response>=>{
     }
     headers.set("X-forwarded-for",fip);
     url.host = toHost;
-    return await fetch(url,{...req,headers:headers}) as any;
+    return await fetch(url,{
+        headers:headers,
+        body:req.body,
+        method:req.method
+    }) as any;
 }
 
 
